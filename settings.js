acdcfc3df44c89966b530a1ba123af060f615a3c
Gullu <129047370+HEROBRINE69H@users.noreply.github.com>
2023-12-29 13:37:48 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,18 +21,18 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.owner = ['919324051764'] //ur owner number
+global.ownernomer = "919324051765" //ur owner number2
+global.ownername = "HEROBRINE" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.socialm = "GitHub: HEROBRINE69H" //ur github or insta name
+global.location = "India, Maharashtra, Mumbai" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
+global.botname = "HEROBRINE"
+global.ownernumber = '919324051765'
+global.ownername = 'HEROBRINE'
+global.ownerNumber = ["919324051765@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
 global.websitex = "https://youtu.be/L_SIk59QeAU"
 global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
